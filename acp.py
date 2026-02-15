ad6cbc1555f96dd63f43fbc19e96f21900f72863
amzva76-pixel <amzva76@gmail.com>
2026-02-15 16:04:43 +0500

    Add files via upload

---
+++ acp.py
@@ -0,0 +1,7 @@
+name = input("Enter the name of the person you want to congratulate: ")
+
+# Display the congratulation message
+print("\n🎉 Congratulations,", name + "! 🎉")
+print("Wishing you continued success and happiness! 🏆")
+
+
